fef9a5e0a9d9f6832f12a8c0d4aae2041d67561f
Sebastian-Mosquera <jsmosquerab@ufpso.edu.co>
2024-05-17 17:42:23 -0500

    FAQ

---
+++ src/components/vue/FAQ.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+
+const data = ['Tema 1', 'Tema 2', 'Tema 3', 'Tema 4']
+
+const selectedIndex = ref(0)
+
+function showInfo(i) {
+  if(i == selectedIndex.value) {
+    selectedIndex.value = -1
+  } else {
+    selectedIndex.value = i
+  }
+}
+</script>
+<template>
+    <div
+      class="bg-blue-500 p-1 max-w-60 mx-auto mt-12"
+    >
+      <div
+        class="mb-1"
+        v-for="(theme, i) in data"
+        :key="i"
+      >
+        <h1 
+          @click="showInfo(i)"
+          class="select-none hover:cursor-pointer bg-blue-400 text-white px-1 flex justify-between"
+          :class="{'bg-blue-800': selectedIndex == i}"
+        >
+          {{ theme }}
+          <span>{{ selectedIndex == i ? '-' : '+' }}</span>
+        </h1>
+        <div
+          v-show="selectedIndex == i"
+          class="bg-blue-50/50 p-1 mt-1"
+        >
+          Texto del tema 1
+        </div>
+      </div>
+    </div> 
+</template>
